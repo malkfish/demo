63ef6a03c218d9b8d813d35b0ca6131fd3c55026
nicole <nwij0698@uni.sydney.edu.au>
2024-09-25 15:18:24 +1000

    added frontend (react)

--- app.js
+++ app.js
@@ -1,36 +1,86 @@
-import fetch from node-fetch;
-
 const express = require('express');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 const CLIENT_ID = require('./private/keys').CLIENT_ID;
 const CLIENT_SECRET = require('./private/keys').CLIENT_SECRET;
-const REDIRECT_URI = "https://localhost:3000/callback"
+const REDIRECT_URI = "http://localhost:3000/callback"
+
+global.access_token;
 
 app.get('/', function(req, res) {
     res.send("Hello world!");
 })
 
 app.get('/login', function(req, res) {
-    var state = generateRandomString(16);
+    //var state = generateRandomString(16);
+    console.log("Reached login page.")
+    console.log("Test")
 
-    var auth_query_parameters = {      
+    var auth_query_parameters = new URLSearchParams({   
+        client_id: CLIENT_ID,   
         response_type: 'code',
-        client_id: CLIENT_ID,
         redirect_uri: REDIRECT_URI,
-        state: state
-    };
+        //state: state
+    });
+
+    console.log(auth_query_parameters)
 
     res.redirect("https://accounts.spotify.com/authorize?" +
-        auth_query_parameters.toString());
+        auth_query_parameters.toString()
+    );
 })
 
-app.get('/callback', function(req, res) {
-    const code = req.queury.code;
-    console.log(code)
+app.get('/callback', async function(req, res) {
+    const code = req.query.code || null;
+    //var state = req.query.state || null;
+
+    console.log("Reached callback.")
+    console.log("Code: "+code)
+    //console.log(state)
+
+    var body = new URLSearchParams({
+        code: code,
+        redirect_uri: REDIRECT_URI,
+        grant_type: "authorization_code",
+      });
     
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "post",
+        body: body,
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+          Authorization:
+            "Basic " +
+            Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
+        },
+    });
+    
+    const data = await response.json();
+    global.access_token = data.access_token;
+    
+    res.redirect("/dashboard");
+})
+
+async function getData(endpoint) {
+    const response = await fetch("https://api.spotify.com/v1" + endpoint, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + global.access_token,
+      },
+    });
+  
+    const data = await response.json();
+    return data;
+  }
+
+app.get('/dashboard', async function(req, res) {
+    const myData = await getData("/me");
+
+    console.log(myData);
+    res.send("Dashboard reached.")
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
